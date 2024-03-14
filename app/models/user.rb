a8e7930e52454580a569c6cd8a426911ef935eff
naka-nana <harunanapanda@icloud.com>
2024-03-14 18:24:53 +0900

    ユーザー管理機能と単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :histories
+  validate :password_complexity
+
+  def password_complexity
+    return if password.blank? || password =~ /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  
+    errors.add(:password, 'is invalid')
+  end
+  validates :nickname, length: { maximum: 6 }, presence: true
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }, presence: true
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }, presence: true
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
+  validates :birth_date, presence: true
 end
