38f8b119aea1382f22236288aab75197cd582443
naka-nana <harunanapanda@icloud.com>
2024-03-16 15:16:07 +0900

    謎

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
+=======
+  root to: 'items#index'
+  resources :items
+>>>>>>> Stashed changes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
