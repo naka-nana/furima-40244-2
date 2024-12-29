8f3fc6e72f72c90cd0cd0ff0071fd4b489b9a7f9
naka-nana <harunanapanda@icloud.com>
2024-12-29 10:59:27 +0900

    売り切れ表示実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,37 +2,53 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
   before_action :set_item
   def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @order = @item.order
     @order_address = OrderAddress.new
   end
 
   def create
-    Rails.logger.debug "Parameters: #{params.inspect}"
-    @order_address = OrderAddress.new(order_address_params)
+    @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       @order_address.save
-      redirect_to root_path, notice: '購入が完了しました'
+      redirect_to root_path
     else
-      flash.now[:alert] = '入力内容を確認してください'
-      Rails.logger.debug @order_address.errors.full_messages
-      render :index, status: :unprocessable_entity
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+      render 'index'
     end
   end
 
   private
 
-  def set_item
-    @item = Item.find(params[:item_id])
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy' # 通貨の種類（日本円）
+    )
   end
 
   def order_address_params
-    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone).merge(
+    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone, :token).merge(
       user_id: current_user.id,
       item_id: @item.id
     )
   end
 
   def address_params
-    params.require(:address).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone)
+    params.require(:order).permit(:price).merge(token: params[:token])
+  end
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, alert: '指定された商品が見つかりませんでした。'
   end
 end
